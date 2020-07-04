9f14df19c182bb47e60714505da08d53b5f9fa26
Roman <pondz.rf@mail.ru>
2020-07-04 15:04:12 +0300

    Initial commit

---
+++ SmackChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmackChat
+//
+//  Created by Roman Tuzhilkin on 7/3/20.
+//  Copyright © 2020 Roman Tuzhilkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
